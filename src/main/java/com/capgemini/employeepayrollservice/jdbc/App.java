593a5e2e4900f32888abdf13a75fa1e3a312c5a2
123akg <abhijeetkumargiri7070@gmail.com>
2020-10-29 00:52:39 +0530

    Added: Master Branch

---
+++ src/main/java/com/capgemini/employeepayrollservice/jdbc/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.employeepayrollservice.jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
